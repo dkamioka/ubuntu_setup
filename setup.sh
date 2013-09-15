9d2f4fb81c38b8ccd9183b7fdb4d4c5761590157
Diogo Key <diogo.kamioka@gmail.com>
2013-09-15 15:57:11 +0000

    Adicionando os scripts de setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#
+#sudo apt-get update
+#sudo apt-get install git -f
+#sudo apt-get install -f curl
+#sudo apt-get install -f tree
+#\curl -L https://get.rvm.io | bash -s stable
+#source ~/.profile
+#rvm install 1.9.3
+#rvm install 2.0.0
+rvm alias create default 2.0.0
+cp .gitconfig ~/.gitconfig
