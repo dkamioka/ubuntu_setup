f56dc974267583abc7cda4c5bf3c1df36824a922
Diogo Key <diogo.kamioka@gmail.com>
2013-09-17 16:37:45 +0000

    Adicionando o config do PS1 mas ainda nao esta funcionando

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
-#
-#sudo apt-get update
-#sudo apt-get install git -f
-#sudo apt-get install -f curl
-#sudo apt-get install -f tree
-#\curl -L https://get.rvm.io | bash -s stable
-#source ~/.profile
-#rvm install 1.9.3
-#rvm install 2.0.0
+
+sudo apt-get update
+sudo apt-get install git -f
+sudo apt-get install -f curl
+sudo apt-get install -f tree
+\curl -L https://get.rvm.io | bash -s stable
+source ~/.profile
+rvm install 1.9.3
+rvm install 2.0.0
 rvm alias create default 2.0.0
 cp .gitconfig ~/.gitconfig
+cp ps1_git ~/.ps1_git
+echo "source ~/.ps1_git" >> ~/.bashrc
